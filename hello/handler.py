2af23f1ec75b8f864f6d2118b8845dea366e69c0
Utsav Anand <utsavanand2@gmail.com>
2020-05-17 19:04:11 +0530

    Change HTTP response msg
    Signed-off-by: Utsav Anand <utsavanand2@gmail.com>

--- hello/handler.py
+++ hello/handler.py
@@ -2,5 +2,5 @@ def handle(event, context):
     # TODO implement
     return {
         "statusCode": 200,
-        "body": "Hello from Utsav's Pi Cluster!"
+        "body": "Hello from Utsav's GKE Cluster!"
     }
